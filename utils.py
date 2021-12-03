49c77f78abd74a19cb5e876c71e93bfb0a5961b3
ursueugen <ursu_eugen@hotmail.com>
2021-12-03 17:33:49 +0200

    Organized notebook code.

---
+++ utils.py
@@ -0,0 +1,32 @@
+from typing import Set
+import networkx as nx
+import pandas as pd
+
+
+GENE_NAME_A_COL = "Official Symbol Interactor A"
+GENE_NAME_B_COL = "Official Symbol Interactor B"
+
+
+def load_biogrid() -> pd.DataFrame:
+    return pd.read_csv("BIOGRID-ORGANISM-Caenorhabditis_elegans-4.4.203.tab3.txt", sep='\t')
+
+def get_biogrid_gene_names(df_biogrid: pd.DataFrame) -> Set[str]:
+    return set(df_biogrid[GENE_NAME_A_COL]).union(df_biogrid[GENE_NAME_B_COL])
+
+
+def load_synergyage() -> pd.DataFrame:
+    return pd.read_csv("synergyage_database_augmented.tsv", sep='\t')
+
+def get_synergyage_genes(df_synergy: pd.DataFrame) -> Set[str]:
+    genes_syn = set([])
+    for genes_str in df_syn["genes_str"]:
+        genes_syn = genes_syn.union( set( genes_str.split(";") ) )
+    return genes_syn
+
+
+def graph_from_biogrid(df_biogrid: pd.DataFrame) -> nx.Graph:
+    return nx.from_pandas_edgelist(
+        df_biogrid[[GENE_NAME_A_COL, GENE_NAME_B_COL]], 
+        GENE_NAME_A_COL, 
+        GENE_NAME_B_COL
+    )
